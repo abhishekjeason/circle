1b40b9a047838f09efe4a4871ca9482ce8395e05
Abhishek Jeason <66827682+abhishekjeason@users.noreply.github.com>
2020-06-18 23:06:15 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,3 +1,6 @@
 pi=3.14159
 r=float(input("Enter the radius of the circle:"))
 print("The area of the circle is " +str(pi*r**2))
+Output:
+Enter the radius of the circle:1.1
+The area of the circle is 3.8013239000000003
