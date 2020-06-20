63cfb43c62e00396f713138ffa0c49530c2f3454
Abhishek Jeason <66827682+abhishekjeason@users.noreply.github.com>
2020-06-20 18:42:18 +0530

    Update Positive_Number.py

--- Positive_Number.py
+++ Positive_Number.py
@@ -1,4 +1,7 @@
-list1 = [1, -304, 63, 0, 101, 7] 
-for number in list1: 
+a = [1, -304, 63, 0, 101, 7] 
+for number in a: 
       if number >= 0: 
        print(number, end = " ")
+
+Output:
+1 63 0 101 7 
