001647a7135a9211989eae0fd4b4761547f783f4
Abhishek Jeason <66827682+abhishekjeason@users.noreply.github.com>
2020-06-20 18:48:56 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
-Number = int(input("\nPlease Enter the Range Number: "))
+Number = int(input("\nEnter the Range Number: "))
 First_number = 0
 Second_number = 1
 for Num in range(0, Number):
@@ -9,3 +9,11 @@ for Num in range(0, Number):
                       First_number = Second_number
                       Second_number = Next
            print(Next)
+           
+Output:
+Enter the Range Number: 5
+0
+1
+1
+2
+3
